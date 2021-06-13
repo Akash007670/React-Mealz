21b9b4785530536f89f8d17757a6c5727a996552
Akash Kumar <akaysharma894@gmail.com>
2021-06-13 22:37:04 +0530

    Added meals summary

---
+++ src/Meals/MealsSummary.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./MealsSummary.css";
+
+const MealsSummary = () => {
+  return (
+    <>
+      <section className="summary">
+        <h2>Delecious Food, Deleivered to you.</h2>
+        <p>
+          Choose your favorite meal from our brand selection of available meals
+          and enjoy a delecious lunch or dinner at home.
+        </p>
+        <p>
+          All our meals are cooked with high quality ingredients, just in time
+          and ofcourse by top chefs.
+        </p>
+      </section>
+    </>
+  );
+};
+
+export default MealsSummary;
