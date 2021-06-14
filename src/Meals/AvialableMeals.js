f19965e6ba6fa20f416c5ea2e4738bc726f2d5f2
Akash Kumar <akaysharma894@gmail.com>
2021-06-14 23:38:42 +0530

    updated

--- src/Meals/AvialableMeals.js
+++ src/Meals/AvialableMeals.js
@@ -1,29 +1,30 @@
 import React from "react";
 import Card from "../UI/Card";
 import "./AvialableMeals.css";
+import MealItem from "./MealItem/MealItem";
 const DUMMY_DATA = [
   {
     id: "r1",
-    name: "chicken-biriyani",
-    price: "Rs 500",
+    name: "Chicken-biriyani",
+    price: "Rs.500",
     description: "A Food item which is made by chicken,rice and mixed spices.",
   },
   {
     id: "r2",
     name: "Butter-chicken",
-    price: "Rs 800",
+    price: "Rs.800",
     description: "A Food item which is loved most with naan.",
   },
   {
     id: "r3",
     name: "Burger",
-    price: "Rs 100",
+    price: "Rs.100",
     description: "A snack item with full of vegies.",
   },
   {
     id: "r4",
     name: "Pizza",
-    price: "Rs 1000",
+    price: "Rs.1000",
     description: "A snack item with lots of cheese and love.",
   },
 ];
@@ -34,8 +35,14 @@ const AvialableMeals = () => {
       <section className="meals">
         <Card>
           <ul>
-            {DUMMY_DATA.map((data) => (
-              <li key={data.id}>{data.name}</li>
+            {DUMMY_DATA.map((meal) => (
+              // <li key={data.id}>{data.name}</li>
+              <MealItem
+                key={meal.id}
+                name={meal.name}
+                price={meal.price}
+                description={meal.description}
+              />
             ))}
           </ul>
         </Card>
