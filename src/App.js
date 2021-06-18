89da71d7bf00963ab991b876305dd94591ce34b2
Akash Kumar <akaysharma894@gmail.com>
2021-06-19 00:01:00 +0530

    Added states to show or hide cart modal

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Cart from "./Cart/Cart";
 import Header from "./Layout/Header";
 import Meals from "./Meals/Meals";
 
 const App = () => {
+  const [cartShowItem, setCartShowItem] = useState(false);
+
+  const showCartHandler = () => {
+    setCartShowItem(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartShowItem(false);
+  };
+
   return (
     <>
-      <Header />
-      <Cart />
+      {cartShowItem && <Cart onHideCart={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main className="main-container">
         <Meals />
       </main>
-      <Cart />
     </>
   );
 };
